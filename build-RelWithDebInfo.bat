69ed3afd2c64f7a4d19559d76a4a3c7072dcac83
gmh <2315157@qq.com>
2022-03-31 19:00:26 +0800

    Delete build-RelWithDebInfo.bat

--- build-RelWithDebInfo.bat
+++
@@ -1,8 +0,0 @@
-cd ./
-mkdir build-RelWithDebInfo
-cd build-RelWithDebInfo
-cmake ../ -DLLVM_DIR=E:\llvm\llvm-msvc\build-RelWithDebInfo-64\lib\cmake\llvm -DClang_DIR=E:\llvm\llvm-msvc\build-RelWithDebInfo-64\lib\cmake\clang
-cd ../
-
-
-
