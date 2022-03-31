12f25c67cbfb19207c3008789d6a8d52bcf07253
gmh <2315157@qq.com>
2022-03-31 19:00:35 +0800

    Delete build-debug.bat

--- build-debug.bat
+++
@@ -1,8 +0,0 @@
-cd ./
-mkdir build-debug
-cd build-debug
-cmake ../ -DLLVM_DIR=E:\llvm\llvm-msvc\build-debug-64\lib\cmake\llvm -DClang_DIR=E:\llvm\llvm-msvc\build-debug-64\lib\cmake\clang -DDEBUG_MODE=true
-cd ../
-
-
-
